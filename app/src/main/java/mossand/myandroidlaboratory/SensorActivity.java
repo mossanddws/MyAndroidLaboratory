da489a5fa2891085dfe4eec4e6a2055195d037b5
Mossand <mossand.dws@outlook.com>
2018-11-03 18:21:28 +0100

    RotationVector
    Try to show the values of the RotationVector Sensor.

--- app/src/main/java/mossand/myandroidlaboratory/SensorActivity.java
+++ app/src/main/java/mossand/myandroidlaboratory/SensorActivity.java
@@ -20,13 +20,19 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     private TextView linearAccelerometerX;
     private TextView linearAccelerometerY;
     private TextView linearAccelerometerZ;
+    private TextView rotationVectorX;
+    private TextView rotationVectorY;
+    private TextView rotationVectorZ;
+    private TextView rotationVectorScalar;
 
     private SensorManager sensorManager;
     private Sensor gravitySensor;
     private Sensor linearAccelerometerSensor;
+    private Sensor rotationVectorSensor;
 
     private float[] gravity;
     private float[] linearAccelerometer;
+    private float[] rotationVector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +54,7 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         super.onResume();
         sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(this, linearAccelerometerSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, rotationVectorSensor, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
@@ -64,6 +71,9 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
             case Sensor.TYPE_LINEAR_ACCELERATION:
                 showLinearAccelerometer(sensorEvent.values);
                 break;
+            case Sensor.TYPE_ROTATION_VECTOR:
+                showRotationVector(sensorEvent.values);
+                break;
         }
     }
 
@@ -74,6 +84,10 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         linearAccelerometerX = findViewById(R.id.textView_LinearAccelerometerX);
         linearAccelerometerY = findViewById(R.id.textView_LinearAccelerometerY);
         linearAccelerometerZ = findViewById(R.id.textView_LinearAccelerometerZ);
+        rotationVectorX = findViewById(R.id.textView_RotationVectorX);
+        rotationVectorY = findViewById(R.id.textView_RotationVectorY);
+        rotationVectorZ = findViewById(R.id.textView_RotationVectorZ);
+        rotationVectorScalar = findViewById(R.id.textView_RotationVectorScalar);
     }
 
     private void initSensors() {
@@ -81,12 +95,15 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         //identifySensors();
         gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         linearAccelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
     }
 
     private void initData() {
         gravity = new float[3];
         linearAccelerometer = new float[3];
+        rotationVector = new float[4];
     }
+
     /*
     private void identifySensors() {
         List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
@@ -101,30 +118,49 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     private void showGravity(float[] values) {
         if (gravity[0] != values[0]) {
             gravity[0] = values[0];
-            gravityX.setText(String.format(Locale.getDefault(), "%.2f", gravity[0]));
+            gravityX.setText(String.format(Locale.getDefault(), "%.3f", gravity[0]));
         }
         if (gravity[1] != values[1]) {
             gravity[1] = values[1];
-            gravityY.setText(String.format(Locale.getDefault(), "%.2f", gravity[1]));
+            gravityY.setText(String.format(Locale.getDefault(), "%.3f", gravity[1]));
         }
         if (gravity[2] != values[2]) {
             gravity[2] = values[2];
-            gravityZ.setText(String.format(Locale.getDefault(), "%.2f", gravity[2]));
+            gravityZ.setText(String.format(Locale.getDefault(), "%.3f", gravity[2]));
         }
     }
 
     private void showLinearAccelerometer(float[] values) {
         if (linearAccelerometer[0] != values[0]) {
             linearAccelerometer[0] = values[0];
-            linearAccelerometerX.setText(String.format(Locale.getDefault(), "%.2f", linearAccelerometer[0]));
+            linearAccelerometerX.setText(String.format(Locale.getDefault(), "%.3f", linearAccelerometer[0]));
         }
         if (linearAccelerometer[1] != values[1]) {
             linearAccelerometer[1] = values[1];
-            linearAccelerometerY.setText(String.format(Locale.getDefault(), "%.2f", linearAccelerometer[1]));
+            linearAccelerometerY.setText(String.format(Locale.getDefault(), "%.3f", linearAccelerometer[1]));
         }
         if (linearAccelerometer[2] != values[2]) {
             linearAccelerometer[2] = values[2];
-            linearAccelerometerZ.setText(String.format(Locale.getDefault(), "%.2f", linearAccelerometer[2]));
+            linearAccelerometerZ.setText(String.format(Locale.getDefault(), "%.3f", linearAccelerometer[2]));
+        }
+    }
+
+    private void showRotationVector(float[] values){
+        if(rotationVector[0]!=values[0]){
+            rotationVector[0]=values[0];
+            rotationVectorX.setText(String.format(Locale.getDefault(), "%.3f", rotationVector[0]));
+        }
+        if(rotationVector[1]!=values[1]){
+            rotationVector[1]=values[1];
+            rotationVectorY.setText(String.format(Locale.getDefault(), "%.3f", rotationVector[1]));
+        }
+        if(rotationVector[2]!=values[2]){
+            rotationVector[2]=values[2];
+            rotationVectorZ.setText(String.format(Locale.getDefault(), "%.3f", rotationVector[2]));
+        }
+        if(rotationVector[3]!=values[3]){
+            rotationVector[3]=values[3];
+            rotationVectorScalar.setText(String.format(Locale.getDefault(), "%.3f", rotationVector[3]));
         }
     }
 }
