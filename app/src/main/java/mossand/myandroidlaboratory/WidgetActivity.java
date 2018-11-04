5b91e05e5171e387fbe2fa6ee7cb8052330c693e
Mossand <mossand.dws@outlook.com>
2018-11-04 23:16:12 +0100

    Add some widgets
    Add some widgets

---
+++ app/src/main/java/mossand/myandroidlaboratory/WidgetActivity.java
@@ -0,0 +1,16 @@
+package mossand.myandroidlaboratory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WidgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget);
+
+
+    }
+}
